eb05d096c969fb862b7f8bed81bec07ecc52f787
BOVindy <benvierhile@gmail.com>
2019-05-28 15:09:58 -0400

    refactored to use defined functions

--- main.py
+++ main.py
@@ -31,36 +31,75 @@ Date/Time
 # print(notebook)
 
 
+# counter = 1
+# notebook = []
+# now = date.today()
+
+# while True:
+#     user_response = input("what would you like to do? \n>"
+#                         "1. add a note \n"
+#                         "2. print a note \n"
+#                         "3. exit\n> "
+#         )
+
+
+#     if user_response == "1":
+#         content = input("What is the note\n>")
+#         note_id = counter
+#         note = (note_id, str(now), content)
+#         notebook.append(note)
+#         counter += 1
+#     if user_response == "2":
+#         for note in notebook:
+#             print(f"ID: {note[0]}| {note[2]}")
+#     elif user_response == "3":
+#         exit()
+#     else:
+#         print("invalid input")
+
+# # while counter <= 5:
+# #     content = input("What is the note\n>")
+# #     note_id = counter
+# #     note = (note_id, str(now), content)
+# #     notebook.append(note)
+# #     counter += 1
+# #     print(notebook)
+
+
 counter = 1
 notebook = []
 now = date.today()
 
-while True:
+def menu():
     user_response = input("what would you like to do? \n>"
                         "1. add a note \n"
                         "2. print a note \n"
-                        "3. exit\n> "
-        )
+                        "3. exit\n> ")
+    return user_response
 
-
-    if user_response == "1":
+def note_create():
         content = input("What is the note\n>")
+        global counter
         note_id = counter
         note = (note_id, str(now), content)
         notebook.append(note)
-        counter += 1
-    if user_response == "2":
-        for note in notebook:
-            print(f"ID: {note[0]}| {note[2]}")
-    elif user_response == "3":
-        exit()
-    else:
-        print("invalid input")
+        counter += 1    
 
-# while counter <= 5:
-#     content = input("What is the note\n>")
-#     note_id = counter
-#     note = (note_id, str(now), content)
-#     notebook.append(note)
-#     counter += 1
-#     print(notebook)
+def print_notes():
+    for note in notebook:
+        print(f"ID: {note[0]}| {note[2]}")
+
+def run():
+    while True:
+        choice = menu()
+        if user_response == "1":
+            note_create()
+        if user_response == "2":
+            print_notes()
+        elif user_response == "3":
+            exit()
+        else:
+            print("invalid input")
+
+if __name__ == "__main__":
+    run()
